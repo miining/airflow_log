f5c303601110dcafc0f612c6654893bb1cdacf3f
minigit <science1513@gmail.com>
2023-10-10 19:37:30 +0900

    select_fruit 파일 재업로드

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -12,14 +12,14 @@ with DAG(
 ) as dag:
     
     t1_orange = BashOperator(
-        task_id="t1_orage"
-        bash_command="/opt/airflow_log/plugins/shell/select_fruit.sh ORANGE",
+        task_id="t1_orage",
+        bash_command="/opt/airflow_log/plugins/select_fruit.sh ORANGE",
         #opt는 옵션을 의미함
     )
     
     t2_banana = BashOperator(
-        task_id="t2_banana"
-        bash_command="/opt/airflow_log/plugins/shell/select_fruit.sh BANANA",
+        task_id="t2_banana",
+        bash_command="/opt/airflow_log/plugins/select_fruit.sh BANANA",
     )
 
     t1_orange >> t2_banana
