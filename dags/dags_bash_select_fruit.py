3723766343a987ecfe22d81312f15fa1e3a279eb
minigit <science1513@gmail.com>
2023-10-10 19:19:53 +0900

    select_fruit.sh 수행하는 dag 업로드

---
+++ dags/dags_bash_select_fruit.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+
+
+with DAG(
+    dag_id="dags_bash_select_fruit", 
+    schedule="10 0 * * 6#1",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    t1_orange = BashOperator(
+        task_id="t1_orage"
+        bash_command="/opt/airflow_log/plugins/shell/select_fruit.sh ORANGE",
+        #opt는 옵션을 의미함
+    )
+    
+    t2_banana = BashOperator(
+        task_id="t2_banana"
+        bash_command="/opt/airflow_log/plugins/shell/select_fruit.sh BANANA",
+    )
+
+    t1_orange >> t2_banana
