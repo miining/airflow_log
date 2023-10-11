af8fe684c5a9e29badcc2f793653ffddc4006fcb
JSM <science1513@gmail.com>
2023-10-11 11:08:57 +0900

    plugins 폴더 추가

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,12 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ];then
+        echo "selected apple"
+elif [ $FRUIT == ORANGE ];then
+        echo "selected orage"
+elif [ $FRUIT == GRAPE ];then
+        echo "selected grape"
+else
+        echo " you selected other fruit!"
+
+
+fi
