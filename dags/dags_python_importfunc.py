8807aa4d62e801a1a8aaea66ae02babcd83f0b23
minigit <science1513@gmail.com>
2023-10-19 21:46:53 +0900

    파일이 안됨

--- dags/dags_python_importfunc.py
+++ dags/dags_python_importfunc.py
@@ -8,7 +8,7 @@ from common.common_func import get_sftp
 with DAG(
    
     dag_id="dags_python_operator", 
-    schedule="30 5 * * *",
+    schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
