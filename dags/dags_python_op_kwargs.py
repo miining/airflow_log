c24d036ce0bb254fa0f5afc2b4c8a6311d247bb3
minigit <science1513@gmail.com>
2023-11-07 12:28:14 +0900

    update

--- dags/dags_python_op_kwargs.py
+++ dags/dags_python_op_kwargs.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import pendulum
 import datetime
 from airflow.operators.python import PythonOperator
-from common.common_func import regist2
+from common_func import regist2
 
 with DAG(
     dag_id="dags_python_op_kwargs",
@@ -14,8 +14,8 @@ with DAG(
     regist_t2 = PythonOperator(
         task_id = 'regist_t2',
         python_callable=regist2,
-        op_args=['min','kr','ulsan'],
-        op_kwargs={'email':'aaa@naver.com','phone':'010'}
+        op_args=['min','kr','seoul'],
+        op_kwargs={'email':'aod@naver.com','phone':'010'}
     )
     
 regist_t2
