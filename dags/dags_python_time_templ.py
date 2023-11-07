31814b6fb66f5d02dda257006a9721e539f447d9
minigit <science1513@gmail.com>
2023-11-07 20:34:13 +0900

    python templates

---
+++ dags/dags_python_time_templ.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.decorators import task
+
+with DAG(
+   
+    dag_id="dags_python_time_templ", 
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2023, 11, 5, tz="Asia/Seoul"),
+    catchup=True,
+    #11월 5일부터 현재까지 task를 모두 수행
+) as dag:
+    
+    @task(task_id ='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+    
+    show_templates()
+    
