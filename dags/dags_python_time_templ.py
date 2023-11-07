dc9526e5504fb91726c1b0273a08df337e21c34f
minigit <science1513@gmail.com>
2023-11-07 21:08:11 +0900

    template upgrade:

--- dags/dags_python_time_templ.py
+++ dags/dags_python_time_templ.py
@@ -7,7 +7,7 @@ with DAG(
    
     dag_id="dags_python_time_templ", 
     schedule="30 9 * * *",
-    start_date=pendulum.datetime(2023, 11, 5, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2023, 11, 4, tz="Asia/Seoul"),
     catchup=True,
     #11월 5일부터 현재까지 task를 모두 수행
 ) as dag:
@@ -18,4 +18,3 @@ with DAG(
         pprint(kwargs)
     
     show_templates()
-    
