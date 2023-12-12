321faf7b0acd94fa6f3a57a60fb1e634b428e825
minigit <science1513@gmail.com>
2023-12-12 13:22:56 +0900

    date 수정

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_with_xcom",
     schedule="10 0 * * *",
-    start_date=pendulum.datetime(2023,3,1,tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2023,12,1,tz="Asia/Seoul"),
     catchup=False
 )as dag:
     bash_push = BashOperator(
