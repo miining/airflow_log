8147cb9acc77a829a7a281118cc0466710e82972
minigit <science1513@gmail.com>
2024-05-17 13:19:41 +0900

    change conn id

--- dags/dags_python_sensor.py
+++ dags/dags_python_sensor.py
@@ -44,7 +44,7 @@ with DAG(
         python_callable= check_api_update,
         op_kwargs={
             'http_conn_id':'openapi.seoul.go.kr',
-            'endpoint':'{{var.value.apikey_openapi_seoul_go_kr}}/json/',
+            'endpoint':'{{var.value.apikey_openapi_seoul_go_kr}}/json/TbCorona19CountStatus',
             'base_dt_col':'S_DT'
         },
         poke_interval = 600, #10분
